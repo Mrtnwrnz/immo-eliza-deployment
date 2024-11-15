ff3123abb83603c029fddae5a62ca3705b3f9472
Maarten Warnez <mrtnwrnz@gmail.com>
2024-11-15 16:43:05 +0100

    fix import

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title("Immo Eliza's price predictor")
 
 # import csv from scraping
-df = pd.read_csv("../data/properties.csv")
+df = pd.read_csv("properties.csv")
 
 # check for duplicates
 df = duplicates(df)
